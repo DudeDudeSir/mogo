b29960b78e5395a8067df95fed533b216022c42f
RAHUL <88625008+DudeDudeSir@users.noreply.github.com>
2022-12-26 16:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://DudeDudesir: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@github.com/DudeDudeSir/braun-eva-o okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
